ba1e152e5ebb897af1d31a6ee66d5edd3b662d97
Drslope-99 <enamsifon@gmail.com>
2025-09-29 16:19:54 +0100

    added the microphone component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,20 +1,38 @@
 import { useState } from "react";
 import styles from "./Search.module.css";
 import { IoSearch } from "react-icons/io5";
+import { FaMicrophone } from "react-icons/fa";
+import { FaMicrophoneSlash } from "react-icons/fa";
 
 export default function Search() {
   const [search, setSearch] = useState("");
+  const [microphone, setMicrophone] = useState(false);
+
+  const toggleMic = () => {
+    setMicrophone((prev) => !prev);
+  };
+
   return (
     <form className={styles.searchForm}>
       <div className={styles.searchContainer}>
-        <IoSearch className={styles.searchIcon} />
-        <input
-          type="text"
-          className={styles.searchInput}
-          placeholder="Search for a place..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
+        <div className={styles.searchWrapper}>
+          <IoSearch className={styles.searchIcon} />
+          <input
+            type="text"
+            className={styles.searchInput}
+            placeholder="Search for a place..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button
+            type="button"
+            className={`${styles.micBtn} ${microphone ? "active" : ""}`}
+            onClick={toggleMic}
+          >
+            {!microphone ? <FaMicrophoneSlash /> : <FaMicrophone />}
+          </button>
+        </div>
+
         <button type="button" className={styles.searchBtn}>
           search
         </button>
